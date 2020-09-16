9d91b8cafd7ddfb01a1a9f6ece64de5ed94a6545
Alperen <alperenmac21@icloud.com>
2020-09-16 21:59:48 +0300

    carousel,navbar, footer eklendi

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from article.models import Article
+from event.models import Event
 
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(Event)
+
--- article/models.py
+++ article/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.CharField(max_Length=50)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey("auth.user", on_delete=False)
+    author = models.ForeignKey("auth.user", on_delete=models.CASCADE)
